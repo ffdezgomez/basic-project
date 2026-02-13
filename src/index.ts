bec8f0cd3d0b5b87199f26f240131ffe109ca441
Francisco Fdez <alu0101427324@ull.edu.es>
2026-02-13 11:05:28 +0000

    Added basic function

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Function add two **numbers**
+ * 
+ * ```ts
+ * console.log(add(5,6)) // 11
+ * 
+ * ```
+ * 
+ * @param x - First number to add
+ * @param y - Second number tu add
+ * @returns First and second number addition
+ */
+function add(x: number, y: number): number {
+  return x + y
+}
+
+console.log(add(5,6))
+
+
