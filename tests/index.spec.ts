c92f57d6ce354d25363e66bd1bb20ad3bc79e71a
Francisco Fdez <alu0101427324@ull.edu.es>
2026-03-03 13:47:32 +0000

    test: new test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -5,4 +5,8 @@ describe('add function test', () => {
   test('add(30, 15) returns 45', () => {
     expect(add(30, 15)).toBe(45)
   })
+
+  test('add(30, -15) returns 15', () => {
+    expect(add(30, -15)).toBe(15)
+  })
 })
